9bcd3814a6c038cee2cd96bec861bec0b97450ae
xTeKc <bcd3v@pm.me>
2021-05-21 14:53:58 -0400

    update

--- p_web3_02.py
+++ p_web3_02.py
@@ -7,7 +7,7 @@ from web3 import Web3
 
 load_dotenv()
 node_provider2 = os.environ['NODE_PROVIDER'] 
-web3_connection2 = Web3(Web3.HTTPProvider(node_provider))
+web3_connection2 = Web3(Web3.HTTPProvider(node_provider2))
 
 
 contract_address2 = config('CONTRACT_ADDRESS')
